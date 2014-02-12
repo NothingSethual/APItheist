b95581454fc53e07b9a410ed78eb5d87fefecec0
NothingSethual <sethcallaway@gmail.com>
2014-02-11 20:35:49 -0500

    atheist test

---
+++ server.js
@@ -0,0 +1,16 @@
+var query_params = { apikey: 'a12b17ca0d5f2bfba41ee42ec0f051a1', 
+
+phrase:'atheist',
+sort:'desc',
+                   };
+
+var endpoint = 'http://capitolwords.org/api/text.json'
+
+var options = {
+  data: query_params,
+  type: 'GET',
+  dataType: 'jsonp'      
+};
+
+var request = jQuery.ajax(endpoint, options)
+                    .done(showResponse);
