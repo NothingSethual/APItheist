356f7aa2c91b0dc79b525b5cf7df3f6085be5da5
Nothing Sethual <sethcallaway@gmail.com>
2014-02-11 22:08:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-var query_params = { apikey: 'a12b17ca0d5f2bfba41ee42ec0f051a1', 
-
-phrase:'atheist',
-sort:'desc',
+var query_params = { apikey: 'a12b17ca0d5f2bfba41ee42ec0f051a1',
+                     phrase: 'atheist',
                    };
-
-var endpoint = 'http://capitolwords.org/api/text.json'
+var endpoint = 'http://capitolwords.org/api/text.json';
 
 var options = {
   data: query_params,
